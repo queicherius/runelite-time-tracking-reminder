cb2cc1771fa772252fec44abbc92fc27d5256e15
queicherius <queicherius@gmail.com>
2022-04-23 22:53:11 +0100

    Make sure infoboxes have unique names

--- src/main/java/com/timetrackingreminder/TimeTrackingReminderInfoBox.java
+++ src/main/java/com/timetrackingreminder/TimeTrackingReminderInfoBox.java
@@ -7,11 +7,19 @@ import net.runelite.client.plugins.Plugin;
 import net.runelite.client.ui.overlay.infobox.InfoBox;
 
 class TimeTrackingReminderInfoBox extends InfoBox {
+    private final Plugin plugin;
     private final String type;
 
     TimeTrackingReminderInfoBox(Plugin plugin, BufferedImage image, String type) {
         super(image, plugin);
+        this.plugin = plugin;
         this.type = type;
+    }
+
+    @Override
+    public String getName() {
+        String typeString = type.toLowerCase().replaceAll("\\s+", "");
+        return plugin.getClass().getSimpleName() + "_" + getClass().getSimpleName() + "_" + typeString;
     }
 
     @Override
